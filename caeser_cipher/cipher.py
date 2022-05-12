68471c7f86d15b7270ef045c3d21590f72baa9a9
Thomas Basham <Bashamtg@gmail.com>
2022-05-11 18:37:31 -0700

    passsing encrypt

--- caeser_cipher/cipher.py
+++ caeser_cipher/cipher.py
@@ -1,65 +1,63 @@
 import string
-from itertools import cycle
+import re
 
-# alphabet = {
-#         'a' : '1',
-#         'b' : '2',
-#         'c' : '3',
-#         'd' : '4',
-#         'e' : '5',
-#         'f' : '6',
-#         'g' : '7',
-#         'h' : '8',
-#         'i' : '9',
-#         'j' : '10',
-#         'k' : '11',
-#         'l' : '12',
-#         'm' : '13',
-#         'n' : '14',
-#         'o' : '15',
-#         'p' : '16',
-#         'q' : '17',
-#         'r' : '18',
-#         's' : '19',
-#         't' : '20',
-#         'u' : '21',
-#         'v' : '22',
-#         'w' : '23',
-#         'x' : '24',
-#         'y' : '25',
-#         'z' : '26'
-#     }
-# print(alphabet)
 
-alphabet_list = list(string.ascii_lowercase)
-print(alphabet_list)
 
-def encrypt(string, shift):
-    res = []
-    k = shift
+
+def encrypt(message, shift):
+    punctuation_set = list(string.punctuation)
+    alphabet_list = list(string.ascii_lowercase)
+    alphabet_list_upper = list(string.ascii_uppercase)
+    # print(alphabet_list
+
+    shifted_alphabet = []
+
     index_stack = []
     encrypted = []
+
+    shifted = shift
+
+    # Shift the alphabet
     for i in range(len(alphabet_list)):
-        res.append(alphabet_list[k % len(alphabet_list)])
-        k = k + 1
-    for letter in string:
-        if letter in alphabet_list:
-            index_stack.append(alphabet_list.index(letter))
+        shifted_alphabet.append(alphabet_list[shifted % len(alphabet_list)])
+        shifted = shifted + 1
+
+    # retrieve indexes of alphabet
+    for letter in message:
+        if letter.isspace():
+            index_stack.append(letter)
+        if letter.isdigit():
+            index_stack.append(letter)
+        if letter in punctuation_set:
+            index_stack.append(letter)
+        cipher_text = ""
+        if letter.isalpha():
+            stay_in_alpha = ord(letter) + shift
+            if stay_in_alpha > ord('z'):
+                stay_in_alpha -= 26
+            fina_letter = chr(stay_in_alpha)
+            cipher_text += fina_letter
+            index_stack.append(cipher_text)
+
+    print(index_stack)
+
+    # return shifted_alphabet index
     for number in index_stack:
-        encrypted.append(res[number])
+        # if number == " ":
+        #     encrypted.append(number)
+
+        if isinstance(number, str):
+            encrypted.append(number)
+        elif number != " ":
+            encrypted.append(shifted_alphabet[number])
+    # print(punctuation_set)
+
+
 
     return ''.join([str(i) for i in encrypted])
 
+encrypt("Gimme a 1!", 10)
 
-encrypt('apple', 1)
-
-# def encrypt_with_dict(string, shift):
-#     stack = []
-#     temp = list(alphabet)
-#     for letter in string:
-#         if letter in alphabet:
-#             stack.append(temp[temp.index(letter) + shift])
-#     return ''.join([str(i) for i in stack])
 
 def decrypt():
     pass
